1433ae83a56772b42aceffb614b10e55b77aa24a
Robert Tutsek <tutsek_robert@yahoo.com>
2018-04-27 11:02:16 +0300

    Changed JQuery with Angular

--- keywordSearch/keywordSearch/Scripts/YoutubeAngular.js
+++ keywordSearch/keywordSearch/Scripts/YoutubeAngular.js
@@ -1,16 +1,44 @@
 ﻿var youtubeSearchModule = angular.module("YoutubeSearchApp", []);
 
 youtubeSearchModule.controller("YoutubeSearchController", function ($scope, $http) {
+
+    var dataUrl = "/api/YoutubeApi/SearchAsyncPost";
+    var dataToPost = { "Keyword": "", "NamespaceName": "keywordSearchService", "ClassName": "YoutubeSearchLogService", "MethodName": "AddLog" };
+
     $scope.btnPostCall = function () {
-        var dataToSend = { "Keyword": $scope.youtubeSearch, "NamespaceName": "keywordSearchService", "ClassName": "YoutubeSearchLogService", "MethodName": "AddLog" }
-        $http.post("/api/YoutubeApi/SearchAsyncPost", dataToSend).then(function (success) {
+        postData(dataUrl, dataToPost);
+    };
+    $scope.onSearchValKeyup = function (event) {
+        if (event.keyCode == 13) {
+            dataToPost["Keyword"] = $scope.youtubeSearchVal;
+            postData(dataUrl, dataToPost);
+        }
+    };
+    $scope.onThumbnailImageMouseenter = function (event) {
+        $scope.youtubeThumbnailImageBlankerShow = false;
+        angular.element(event.currentTarget).find(".youtubeThumbnailImageBlanker").scope().youtubeThumbnailImageBlankerShow = true;
+    };
+    $scope.onThumbnailImageMouseleave = function () {
+        $scope.youtubeThumbnailImageBlankerShow = false;
+        angular.element(event.currentTarget).find(".youtubeThumbnailImageBlanker").scope().youtubeThumbnailImageBlankerShow = false;
+    };
+    $scope.showVideoModal = function (event) {
+        var videoId = angular.element(event.target).closest(".youtubeThumbnailImageHolder").data('video-id');
+        if (videoId) {
+            var additionalParams = "?controls=0&autoplay=1&showinfo=0&rel=0";
+            angular.element("#youtubeVideoFrame").attr("src", $scope.BaseUrl + "/" + videoId + additionalParams);
+            angular.element("#youtubeVideoModal").modal("show");
+        }      
+    };
+    function postData(url, dataToPost) {
+        dataToPost["Keyword"] = $scope.youtubeSearchVal;
+        $http.post(url, dataToPost).then(function (success) {
             if (success.status == 200) {
                 $scope.BaseUrl = success.data.BaseUrl;
                 $scope.ThumbnailDefaultImage = success.data.ThumbnailDefaultImage;
                 $scope.ThumbnailUrl = success.data.ThumbnailUrl;
                 $scope.YoutubeVideos = success.data.Videos;
-                console.log(success.data);
             }
         });
-    };
+    }
 });
